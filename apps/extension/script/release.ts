68aec0ecfd8896f1287c1e10bed69aa443ae2db8
lychee <lychee@rb2.nl>
2024-08-15 14:19:53 +0800

    feat: Build zip

---
+++ apps/extension/script/release.ts
@@ -0,0 +1,25 @@
+import _config from "../manifest.config";
+import archiver from "archiver";
+import fs from "fs";
+import { resolve } from "node:path";
+
+const { build, name } = _config;
+
+const inputDirPath = resolve(__dirname, "..", build.out);
+
+const outputZipPath = resolve(__dirname, "..", `${name}.zip`);
+
+const output = fs.createWriteStream(outputZipPath);
+const archive = archiver("zip", {
+  zlib: { level: 9 },
+});
+
+archive.on("error", (err) => {
+  throw err;
+});
+
+archive.pipe(output);
+
+archive.directory(inputDirPath, false);
+
+archive.finalize();
