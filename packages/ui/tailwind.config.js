e7d5522cb42887e6f051098b4a76dea1b25be09b
lychee <lychee@rb2.nl>
2024-08-15 14:35:42 +0800

    feat: Add workflow

--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -1,8 +1,4 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  plugins: [
-    // eslint-disable-next-line no-undef
-    require("@tailwindcss/aspect-ratio")
-  ]
-}
+  plugins: [require("@tailwindcss/aspect-ratio")],
+};
